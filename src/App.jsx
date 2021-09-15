1e49168b169552cd6dd4716066caa9e9c56dfdf6
Aadhinana <ajibaa@gmail.com>
2021-09-15 21:51:04 +0530

    chore: artifacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       hello there!
