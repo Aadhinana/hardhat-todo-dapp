50de427cb141ed7995dcc65ddbd30b1f539ca7f6
Aadhinana <ajibaa@gmail.com>
2021-09-15 21:49:56 +0530

    rearrage test

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,17 +2,27 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TodoContarct", function () {
-  it("add a new todo", async function () {
-    const TodoContract = await hre.ethers.getContractFactory("TodoContract");
-    const todoContract = await TodoContract.deploy();
-    await todoContract.deployed();
+  let todoContract;
 
+  // Initialize the contract
+  before(async function () {
+    const TodoContract = await hre.ethers.getContractFactory("TodoContract");
+    todoContract = await TodoContract.deploy();
+    await todoContract.deployed();
+  });
+
+  it("adds and retrieves a new todo", async function () {
     await todoContract.createTodo("First todo", "John Doe");
 
     const getFirstTodo = await todoContract.getTodo(0);
 
-    console.dir(getFirstTodo, { depth: null });
-
+    // console.dir(getFirstTodo, { depth: null });
+    expect(getFirstTodo.content === "First todo");
+    expect(getFirstTodo.author === "John Doe");
     // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+
+  it("throws error message when id does not exist", async function () {
+    await expect(todoContract.getTodo(5)).to.be.revertedWith("Todo with that does not exist");
+  })
 });
